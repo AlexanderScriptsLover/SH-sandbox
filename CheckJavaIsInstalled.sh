510525b377d67791e2bd3686c437497ede42837a
Alexander <sergeevalex1q@gmail.com>
2024-08-26 15:03:16 +0300

    Update CheckJavaIsInstalled.sh with new changes

--- CheckJavaIsInstalled.sh
+++ CheckJavaIsInstalled.sh
@@ -3,11 +3,11 @@
 # Check if Java Is Located
 CheckIfJavaIsLocated() {
 
-if command -v java &> /dev/null
+if command -v java &> /dev/null #command -v java check if command is available, &> /dev/null redirects possible eror message to standard error output
 then 
    echo "Java is installed"
    # Output the version
-   java -version 2>&1 | head -n1
+   java -version 2>&1 | head -n1 #2>&1 redirects possible error messages to standard error output
 else
    echo "No java found, sorry"
 fi
