aabba70de3ff118cad5f87382cf4283293a67935
Alexander <sergeevalex1q@gmail.com>
2024-08-26 21:33:42 +0300

    ValidateArgs script.Updated with comments

--- ValidateArguments.sh
+++ ValidateArguments.sh
@@ -2,9 +2,9 @@
 
 # Look for arguments (should not accept less than two
 checkForArgs() {
-if [ "$#" -lt 2 ]; then
+if [ "$#" -lt 2 ]; then #we check whether it is less than what we need
 echo "I need more, sir!"
-elif [ "$#" -gt 2 ]; then
+elif [ "$#" -gt 2 ]; then #we check whether it is more than what we need
 echo "I need less, sir!"
 else
 echo "Wow, great job!"
