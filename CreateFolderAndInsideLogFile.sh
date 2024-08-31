0ba3687288f1aef27f64f642f140d7d9b44436aa
Alexander <sergeevalex1q@gmail.com>
2024-09-01 00:59:38 +0300

    Create a folder and inside log with a status

---
+++ CreateFolderAndInsideLogFile.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Define places and files
+dir_path="$(pwd)/TestFolder"  # In the current dir we create a folder
+dir_file="$dir_path/Result.log" 
+
+# Create a folder if it does not yet exist
+if [ ! -d "$dir_path" ]; then
+    mkdir "$dir_path"
+
+    # Check if the directory was created successfully
+    if [ $? -ne 0 ]; then
+        echo "Unable to create the folder, please check your permissions or path." | tee -a "$dir_file"
+        exit 1
+    else
+        echo "Folder created successfully." | tee -a "$dir_file"
+    fi
+else
+    echo "Folder already exists." | tee -a "$dir_file"
+fi
+
+# Check if the log file was created
+if [ ! -f "$dir_file" ]; then
+    echo "Unable to create the log file, please check your permissions or path." >&2
+    exit 1
+fi
+
+
+# Status of the last command
+echo "The status of the last command is: $?" | tee -a "$dir_file"
+
